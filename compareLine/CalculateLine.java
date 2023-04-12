8aa049957c078083bd1513d739440e9fd7e1c285
“PrasannaMohanta” <pmohanta53@gmail.com>
2023-04-12 15:49:17 +0530

    Added:calculating the line from two points

---
+++ compareLine/CalculateLine.java
@@ -0,0 +1,34 @@
+package com.compareLine;
+/**
+ *
+ * @author Prasanna
+ *
+ */
+
+public class CalculateLine {
+
+    public static void main(String[] args) {
+
+        /*
+         * point 1
+         */
+        double x1 = 1;
+        double y1 = 1;
+
+        /*
+         * point 2
+         */
+        double x2 = 5;
+        double y2 = 5;
+
+        /*
+         * Calculate distance between two points
+         */
+        double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)
+                * (y2 - y1));
+
+        System.out.println("Distance between two points :" + distance);
+    }
+
+}
+
